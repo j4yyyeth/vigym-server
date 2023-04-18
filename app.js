f35d32296e5504502889563ab35cc121aef324d1
Jacob Handwerger <jstudyeth@gmail.com>
2023-04-18 11:15:58 -0400

    full configuration

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+const { error } = require('console');
 
 var app = express();
+
+app.set('trust proxy', 1);
+app.enable('trust proxy');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +21,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URI]  
+    })
+  );
+
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
+app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
 
 module.exports = app;
